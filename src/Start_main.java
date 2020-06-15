834a3cd66e22c23f06c96e4130fc16326035ef88
donut0310 <donut0310@naver.com>
2020-06-15 14:25:55 +0900

    edit

--- src/Start_main.java
+++ src/Start_main.java
@@ -2,15 +2,7 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -29,15 +21,12 @@ public class Start_main extends JFrame implements ActionListener {
 		super("캠핑카 대여 시스템");
 		//conDB();
 		layInit();
-		Dimension frameSize = getSize();
-	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    	setLocation((screenSize.width - frameSize.width) /2, (screenSize.height - frameSize.height) /2);
-		
-		setSize(200,200);
+		super.setLocationRelativeTo(null);
+
+		setSize(400,200);
 		GridLayout grid = new GridLayout(1,2);
 		setLayout(grid);
 		setVisible(true);
-		// setBounds(750, 400, 200, 200); // 가로위치,세로위치,가로길이,세로길이
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
